8891b709a63f6c7c977018ef549f7616e4f306ac
Lucas Frérot <lucas.frerot@gmail.com>
2020-10-14 10:44:54 -0400

    cleanup of old config files

--- scripts/monitor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-function monifuck() {
-  xrandr --output DP1-2 --off
-  sleep 5
-  xrandr --output DP1-2 --auto --right-of DP1-1
-}
--- scripts/vbox_modules.sh
+++
@@ -1,4 +0,0 @@
-vbox_modules () {
-  sudo emerge virtualbox-modules
-  sudo modprobe vboxdrv vboxnetadp vboxnetflt vboxpci
-}
